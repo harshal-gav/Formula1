165ba3e27f33c61efe9ee013bd3536e8d488cbe4
Harshal Gavali <105380049+harshal-gav@users.noreply.github.com>
2023-03-11 11:32:25 +0530

    first commit

---
+++ src/main/java/com/springBootProject/formula1/repository/DriverRepository.java
@@ -0,0 +1,13 @@
+package com.springBootProject.formula1.repository;
+
+import com.springBootProject.formula1.domain.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long>, JpaSpecificationExecutor<Driver> {
+
+    boolean existsByNameAndYearAndTeam(String name, int year, String team);
+
+}
